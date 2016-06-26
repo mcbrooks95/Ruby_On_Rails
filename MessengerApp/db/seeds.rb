2e144fccec16b99e4982ad6227518e8c388fc499
mcbrooks95 <mcbrooks95@gmail.com>
2016-06-26 16:53:21 -0400

    Added part of the CodeCademy course for rails, able to get and post new messages

---
+++ MessengerApp/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+message1 = Message.create(content: "Hey everyone! How's it going?")
+
+message2 = Message.create(content: "its going great!!!!")
